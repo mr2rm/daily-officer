fd31bff09deba39eb76349b87b6f6a470da00ad0
mrzrm <mohammadreza.rahmani89@gmail.com>
2018-12-03 08:34:46 +0330

    Refactor: move some functions from main.py to functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,46 @@
+import logging
 import sqlite3
 
-from settings import DB_NAME
+from telegram.ext import Updater
+
+import handlers
+from local_settings import TOKEN
+from settings import DB_NAME, HANDLER_SUFFIX, REQUEST_KWARGS
 
 
 def get_db():
 	db = sqlite3.connect(DB_NAME)
 	cursor = db.cursor()
 	return db, cursor
+
+
+def init_db():
+	chats_table_sql = """
+		CREATE TABLE IF NOT EXISTS chats(
+			chat_id TEXT UNIQUE NOT NULL
+		)
+	"""
+
+	db, cursor = get_db()
+	cursor.execute(chats_table_sql)
+	db.commit()
+	db.close()
+
+
+def setup_configs():
+	logging.basicConfig(
+		format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+		level=logging.INFO
+	)
+
+
+def get_bot():
+	updater = Updater(token=TOKEN, request_kwargs=REQUEST_KWARGS)
+	return updater, updater.dispatcher, updater.job_queue
+
+
+def add_handlers(dispatcher):
+	for name in dir(handlers):
+		if name.endswith(HANDLER_SUFFIX):
+			handler = getattr(handlers, name)
+			dispatcher.add_handler(handler)
--- main.py
+++ main.py
@@ -1,43 +1,4 @@
-import logging
-
-from telegram.ext import Updater
-
-import handlers
-from functions import get_db
-from settings import TOKEN, HANDLER_SUFFIX, REQUEST_KWARGS
-
-
-def init_db():
-	chats_table_sql = """
-		CREATE TABLE IF NOT EXISTS chats(
-			chat_id TEXT UNIQUE NOT NULL
-		)
-	"""
-
-	db, cursor = get_db()
-	cursor.execute(chats_table_sql)
-	db.commit()
-	db.close()
-
-
-def setup_configs():
-	logging.basicConfig(
-		format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-		level=logging.INFO
-	)
-
-
-def get_bot():
-	updater = Updater(token=TOKEN, request_kwargs=REQUEST_KWARGS)
-	return updater, updater.dispatcher, updater.job_queue
-
-
-def add_handlers(dispatcher):
-	for name in dir(handlers):
-		if name.endswith(HANDLER_SUFFIX):
-			handler = getattr(handlers, name)
-			dispatcher.add_handler(handler)
-
+from functions import add_handlers, get_bot, setup_configs, init_db
 
 if __name__ == "__main__":
 	init_db()
