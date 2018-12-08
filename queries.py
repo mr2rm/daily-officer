e72284b44e5a8371c0c328cf07bcc11b1ce0593d
mrzrm <mohammadreza.rahmani89@gmail.com>
2018-12-08 10:23:37 +0330

    Chats table updated to store some more data

--- queries.py
+++ queries.py
@@ -1,6 +1,9 @@
 GET_OR_CREATE_CHATS_TABLE = """
 	CREATE TABLE IF NOT EXISTS chats(
-		chat_id INTEGER UNIQUE NOT NULL
+		chat_id INTEGER UNIQUE NOT NULL,
+		is_active BOOLEAN DEFAULT 1,
+		created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+		last_modified_date DATETIME DEFAULT CURRENT_TIMESTAMP
 	)
 """
 
@@ -12,4 +15,9 @@ INSERT_INTO_CHATS_TABLE = """
 GET_CHAT = """
 	SELECT * FROM chats
 	WHERE chat_id = (?)
+"""
+
+GET_ACTIVE_CHATS = """
+	SELECT * FROM chats
+	WHERE is_active = 1
 """
